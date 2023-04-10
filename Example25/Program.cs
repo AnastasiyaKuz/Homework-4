cb0dcecb9e69f157edccfe052050b6ea50a08534
Anastasiya <kuznecovaas94@mail.ru>
2023-04-10 19:28:24 +0300

    Homework 4

---
+++ Example25/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = 1;
+if (numberB < 1)
+{
+    Console.WriteLine("Число В должно быть натуральным");
+}
+else
+{
+    while (numberB!=0)
+    {
+        numberC = numberC * numberA;
+        numberB = numberB - 1;
+    }
+    Console.WriteLine($"Число А в натуральной степени В равно {numberC}");
+}
---
+++ Example27/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int length = Length(numberA);
+Console.Write($"Сумма цифр в числе {numberA} равно {Sum(numberA, length)}");
+int Length(int numberA)
+{
+    int index = 0;
+    while (numberA > 0)
+    {
+        numberA/=10;
+        index++;
+    }
+    return index;
+}
+int Sum(int numberA, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum+=numberA%10;
+        numberA/=10;
+    }
+    return sum;
+}
